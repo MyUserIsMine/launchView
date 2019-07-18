a940cd6d8b387e06648076011c553cc43e4266df
MOYO <498389182@qq.com>
2019-07-18 09:52:10 +0800

    新建

---
+++ LaunchView/LaunchView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LaunchView
+//
+//  Created by MOYO on 2019/7/18.
+//  Copyright © 2019年 MOYO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LaunchView/LaunchView/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LaunchView
+//
+//  Created by MOYO on 2019/7/18.
+//  Copyright © 2019年 MOYO. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
